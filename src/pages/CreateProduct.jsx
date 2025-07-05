b9a2ad4dc3b4d81fc7881c50bc0aa7a5486f3947
Vallabh1807 <panigrahivallabh@gmail.com>
2025-07-05 18:02:10 +0530

    Add product creation form and implement product fetching in Home component

--- src/pages/CreateProduct.jsx
+++ src/pages/CreateProduct.jsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import axios from "axios";
+import React, { useState } from "react";
 
 const CreateProduct = () => {
-  return (
-    <div>CreateProduct</div>
-  )
-}
+  const [product, setProduct] = useState({
+    image: "",
+    title: "",
+    description: "",
+    price: "",
+  });
 
-export default CreateProduct
+  const submitHandler = (e) => {
+    e.preventDefault();
+    axios.post("https://fakestoreapi.co  m/products/",{
+      title:product.title,
+      description:product.description,
+      image:product.image,
+    }).then((res)=>console.log(res.data))
+  };
+  return (
+    <div className="flex flex-col w-full h-[100vh] items-center justify-center">
+      <h1 className="text-4xl font-bold ">Create New Product</h1>
+      <form className="flex flex-col mt-5" onSubmit={submitHandler}>
+        Enter the Image URL:{" "}
+        <input
+          type="url"
+          name=""
+          id=""
+          placeholder="Paste the url"
+          value={product.image}
+          onChange={(e) => setProduct({ ...product, image: e.target.value })}
+        />
+        <br />
+        Enter the Title :{" "}
+        <input
+          type="text"
+          placeholder="Title"
+          value={product.title}
+          onChange={(e) => setProduct({ ...product, title: e.target.value })}
+        />
+        <br />
+        Enter the Description :{" "}
+        <input
+          type="text"
+          placeholder="Description"
+          value={product.description}
+          onChange={(e) =>
+            setProduct({ ...product, description: e.target.value })
+          }
+        />
+        <br />
+        Enter the Price :{" "}
+        <input
+          type="text"
+          placeholder="Price"
+          value={product.price}
+          onChange={(e) => setProduct({ ...product, price: e.target.value })}
+        />
+        <br />
+        <button className="px-4 py-1 bg-green-500">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default CreateProduct;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { FaArrowUpRightFromSquare } from "react-icons/fa6";
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 import { IoEyeOutline } from "react-icons/io5";
@@ -10,28 +11,64 @@ const Home = () => {
   const handleClick = () => {
     navigate("/products");
   };
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [product, setproduct] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://fakestoreapi.com/products/")
+      .then((res) => setproduct(res.data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  const goToPrev = () => {
+    setCurrentIndex((prev) => (prev - 1) % product.length);
+  };
+  const goToNext = () => {
+    setCurrentIndex((prev) => (prev - 1 + product.length) % product.length);
+  };
+  const navigate2 = useNavigate();
+  const goToProduct = () => {
+    const currentProduct = product[currentIndex];
+    navigate2(`/products/${currentProduct.id}`);
+  };
 
   return (
     <>
       <div>
         <div className="h-screen w-full bg-fuchsia-200 pt-10 flex">
-          <div className="h-[450px] w-[320px] rounded-2xl overflow-hidden mx-40 relative">
-            <img
-              className="h-full w-full object-cover"
-              src="https://images.unsplash.com/photo-1515886657613-9f3515b0c78f?q=80&w=420&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              alt="model"
-            />
+          <div className="h-[450px] w-[320px] rounded-2xl overflow-hidden mx-40 relative bg-purple-300">
+            {product.length > 0 && product[currentIndex] ? (
+              <img
+                className="h-full w-full object-contain"
+                src={product[currentIndex].image}
+                alt={product[currentIndex].title}
+              />
+            ) : (
+              <div className="h-full w-full flex items-center justify-center bg-gray-100 text-gray-500">
+                Loading...
+              </div>
+            )}
             <div className="absolute inset-0 z-10 flex items-center justify-between px-4">
               {/* Left Arrow */}
-              <button className="bg-yellow-100 text-black p-2 rounded-full hover:bg-fuchsia-200 shadow cursor-pointer">
+              <button
+                onClick={goToPrev}
+                className="bg-yellow-100 text-black p-2 rounded-full hover:bg-fuchsia-200 shadow cursor-pointer"
+              >
                 <IoIosArrowBack size={24} />
               </button>
-              <button className="bg-yellow-100 text-black p-2 rounded-full hover:bg-fuchsia-200 shadow cursor-pointer">
+              <button
+                onClick={goToNext}
+                className="bg-yellow-100 text-black p-2 rounded-full hover:bg-fuchsia-200 shadow cursor-pointer"
+              >
                 <IoIosArrowForward size={24} />
               </button>
             </div>
             <div className="absolute bottom-4 left-1/2 -translate-x-1/2 z-10">
-              <button className="bg-yellow-100/70 text-black px-2 py-2 rounded-full hover:hover:bg-yellow-100 cursor-pointer shadow-lg">
+              <button
+                onClick={goToProduct}
+                className="bg-yellow-100/70 text-black px-2 py-2 rounded-full hover:hover:bg-yellow-100 cursor-pointer shadow-lg"
+              >
                 <IoEyeOutline />
               </button>
             </div>
@@ -110,7 +147,7 @@ const Home = () => {
               </span>
             </button>
             <button className="bg-zinc-200/80 px-4 py-1 h-50 w-70 relative rounded-3xl active:bg-fuchsia-200 mt-5 font-semibold text-xl">
-              Active Wear 
+              Active Wear
               <span className="absolute bottom-4 left-4 bg-white/70 p-2 rounded-full border-1">
                 <MdArrowOutward size={30} />
               </span>
